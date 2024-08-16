bf7de38c031a89b0e9083c1dd7213e4d13e14a50
Rafael Chen <rchen95@Jh.edu>
2024-08-16 17:23:33 -0400

    import packages and creating flashcard func

--- app/flashcard/page.js
+++ app/flashcard/page.js
@@ -1,1 +1,13 @@
 'use client'
+
+import { useUser } from "@clerk/nextjs"
+import { useEffect, useState } from "react"
+import { collection, doc, getDoc, getDocs } from "firebase/firestore"
+import { db } from "@/firebase"
+import { useSearchParams } from "next/navigation"
+
+export default function Flashcard() {
+    const { isLoaded, isSignedIn, user } = useUser();
+    const [flashcards, setFlashcards] = useState([]);
+    const [flipped, setFlipped] = useState({});
+}

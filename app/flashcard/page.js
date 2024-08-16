381bf6670260e186cd75ea79d7540fbea2030341
Rafael Chen <rafaelchen@Rafaels-MBP-2.lan>
2024-08-16 15:28:05 -0400

    implement searching for flashcards in set

--- app/flashcard/page.js
+++ app/flashcard/page.js
@@ -1,1 +1,34 @@
 'use client';
+
+import { useUser } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+import {collection, doc, getDoc, getDocs} from 'firebase/firestore'
+import { db } from "@/firebase";
+
+import { useSearchParams } from "next/navigation";
+
+export default function Flashcard() {
+    const { isLoaded, isSignedIn, user } = useUser();
+    const [flashcards, setFlashcards] = useState([]);
+    const [flipped, setFlipped] = useState({});
+
+    const searchParams = useSearchParams()
+    const search = searchParams.get('id')
+
+    useEffect(() => {
+        async function getFlashcard() {
+            if (!user || !search) return;
+            
+            const colRef = collection(doc(collection(db, 'users'), user.id), search);
+            const docs = await getDocs(colRef);
+            const flashcards = []
+            
+            docs.forEach((doc=>{
+                flashcards.push({id: doc.id, ...doc.data()})
+            }))
+            setFlashcards(flashcards)
+        }
+        getFlashcard();
+    }, [user, search]);
+
+}

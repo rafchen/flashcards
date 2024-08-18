047043b7f901045d3f045b60d8938be1fe7f550f
Rafael Chen <rchen95@Jh.edu>
2024-08-18 12:51:54 -0400

    minor changes w/middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,8 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
-  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-});
+const clerkConfig = {
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || '',
+  secretKey: process.env.CLERK_SECRET_KEY || '',
+};
+
+export default clerkMiddleware(clerkConfig);
